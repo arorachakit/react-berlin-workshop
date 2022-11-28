65752b377c41bfd94dc88f0ba69c3dfa38ca4f2e
schabibi1 <arisagse@gmail.com>
2022-11-28 13:56:26 +0100

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,24 @@
 import '../styles/globals.css'
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+import Hero from "../components/Hero";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+  hero: Hero
+};
+
+storyblokInit({
+  accessToken: "6AusfH9XjZMFpVtEtEDzTwtt",
+  use: [apiPlugin],
+  components,
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
